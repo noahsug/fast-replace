17f0d614915eec92785c3b5705e86179e35a9903
noah.sugarman <noah.sugarman@airbnb.com>
2018-09-25 10:10:01 -0700

    add prettier

--- __tests__/fastReplace.test.js
+++ __tests__/fastReplace.test.js
@@ -26,7 +26,10 @@ it('finds and replaces text', () => {
 });
 
 it('finds and replaces regex', () => {
-  return fastReplace('([A-Z])([a-z]+)e', 'e$2$1', { quiet: true, paths: ['mockDir'] }).then(() => {
+  return fastReplace('([A-Z])([a-z]+)e', 'e$2$1', {
+    quiet: true,
+    paths: ['mockDir'],
+  }).then(() => {
     expect(fs.readFileSync('mockDir/f1', 'utf8')).toBe('banana');
     expect(fs.readFileSync('mockDir/f2', 'utf8')).toBe('epplA');
   });
@@ -45,36 +48,54 @@ it('respects .gitignore by default', () => {
 });
 
 it('dryrun does not write changes', () => {
-  return fastReplace('a', 'z', { quiet: true, paths: ['mockDir'], dryrun: true }).then(() => {
+  return fastReplace('a', 'z', {
+    quiet: true,
+    paths: ['mockDir'],
+    dryrun: true,
+  }).then(() => {
     expect(fs.readFileSync('mockDir/f1', 'utf8')).toBe('banana');
     expect(fs.readFileSync('mockDir/f2', 'utf8')).toBe('Apple');
   });
 });
 
 it('ignoreCase ignores case', () => {
-  return fastReplace('a[A-Z]', 'z', { quiet: true, paths: ['mockDir'], ignoreCase: true }).then(
-    () => {
-      expect(fs.readFileSync('mockDir/f1', 'utf8')).toBe('bzza');
-      expect(fs.readFileSync('mockDir/f2', 'utf8')).toBe('zple');
-    }
-  );
+  return fastReplace('a[A-Z]', 'z', {
+    quiet: true,
+    paths: ['mockDir'],
+    ignoreCase: true,
+  }).then(() => {
+    expect(fs.readFileSync('mockDir/f1', 'utf8')).toBe('bzza');
+    expect(fs.readFileSync('mockDir/f2', 'utf8')).toBe('zple');
+  });
 });
 
 it('fixedStrings treats pattern as literal string', () => {
-  return fastReplace('.*', '', { quiet: true, paths: ['mockDir'], fixedStrings: true }).then(() => {
+  return fastReplace('.*', '', {
+    quiet: true,
+    paths: ['mockDir'],
+    fixedStrings: true,
+  }).then(() => {
     expect(fs.readFileSync('mockDir/f1', 'utf8')).toBe('banana');
   });
 });
 
 it('globs includes matching files', () => {
-  return fastReplace('.*', '', { quiet: true, paths: ['mockDir'], globs: ['*1'] }).then(() => {
+  return fastReplace('.*', '', {
+    quiet: true,
+    paths: ['mockDir'],
+    globs: ['*1'],
+  }).then(() => {
     expect(fs.readFileSync('mockDir/f1', 'utf8')).toBe('');
     expect(fs.readFileSync('mockDir/f2', 'utf8')).toBe('Apple');
   });
 });
 
 it('ignoreGlobs excludes matching files', () => {
-  return fastReplace('.*', '', { quiet: true, paths: ['mockDir'], ignoreGlobs: ['*1'] }).then(() => {
+  return fastReplace('.*', '', {
+    quiet: true,
+    paths: ['mockDir'],
+    ignoreGlobs: ['*1'],
+  }).then(() => {
     expect(fs.readFileSync('mockDir/f1', 'utf8')).toBe('banana');
     expect(fs.readFileSync('mockDir/f2', 'utf8')).toBe('');
   });
